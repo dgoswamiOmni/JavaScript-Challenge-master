9ceefcd89c001944785d32a5770d534030138e5e
Devarshi Goswami <devarshigoswami@Devarshis-MacBook-Air.local>
2023-11-25 01:27:53 +0000

    final commit

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-const express = require('express')
-const path = require('path')
-const stocks = require('./stocks')
+const express = require('express');
+const path = require('path');
+const stocks = require('./stocks');
 
-const app = express()
-app.use(express.static(path.join(__dirname, 'static')))
+const app = express();
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.get('/stocks', async (req, res) => {
-  const stockSymbols = await stocks.getStocks()
-  res.send({ stockSymbols })
-})
+  try {
+    const stockSymbols = await stocks.getStocks();
+    res.send({ stockSymbols });
+  } catch (error) {
+    console.error('Error fetching stock symbols:', error.message);
+    res.status(500).send({ error: 'Failed to retrieve stock symbols' });
+  }
+});
 
 app.get('/stocks/:symbol', async (req, res) => {
-  const { params: { symbol } } = req
-  const data = await stocks.getStockPoints(symbol, new Date())
-  res.send(data)
-})
+  const { params: { symbol } } = req;
 
-app.listen(3000, () => console.log('Server is running!'))
+  try {
+    const data = await stocks.getStockPoints(symbol, new Date());
+    res.send(data);
+  } catch (error) {
+    console.error(`Error fetching stock data for ${symbol}:`, error.message);
+    
+    if (error.message === 'Failed to generate stock data' || error.message.startsWith('Unknown stock')) {
+      // Return a 404 status for client-side handling
+      res.status(404).send({ error: error.message });
+    } else {
+      // Return a 500 status for other errors
+      res.status(500).send({ error: 'Failed to retrieve stock data' });
+    }
+  }
+});
+
+app.listen(3000, () => console.log('Server is running!'));
--- static/scripts.js
+++ static/scripts.js
@@ -1,24 +1,172 @@
-const canvas = document.getElementById('chart')
-const ctx = canvas.getContext('2d')
+// Create the SVG container only once when the page is loaded.
+const chart = d3.select('#chart');
+const width = 1000;
+const height = 600;
+const margin = { top: 20, right: 20, bottom: 30, left: 40 };
 
-function drawLine (start, end, style) {
-  ctx.beginPath()
-  ctx.strokeStyle = style || 'black'
-  ctx.moveTo(...start)
-  ctx.lineTo(...end)
-  ctx.stroke()
+const svg = chart.append('svg')
+  .attr('width', width)
+  .attr('height', height);
+
+// Function to fill the dropdown with the list of stocks
+function fillStockDropdown(stockList) {
+  const stockDropdown = document.getElementById('stockDropdown');
+
+  // Clear existing options
+  stockDropdown.innerHTML = '';
+
+  // Add new options
+  stockList.forEach(stock => {
+    const option = document.createElement('option');
+    option.value = stock;
+    option.textContent = stock;
+    stockDropdown.appendChild(option);
+  });
 }
 
-function drawTriangle (apex1, apex2, apex3) {
-  ctx.beginPath()
-  ctx.moveTo(...apex1)
-  ctx.lineTo(...apex2)
-  ctx.lineTo(...apex3)
-  ctx.fill()
+// Function to fetch the list of stocks
+async function fetchStocks() {
+  try {
+    const response = await fetch('/stocks');
+    const data = await response.json();
+    return data.stockSymbols;
+  } catch (error) {
+    console.error('Error fetching stock symbols:', error.message);
+    throw error;
+  }
 }
 
-drawLine([50, 50], [50, 550])
-drawTriangle([35, 50], [65, 50], [50, 35])
+// Function to initialize the page
+async function initializePage() {
+  try {
+    console.log('Initializing page...');
 
-drawLine([50, 550], [950, 550])
-drawTriangle([950, 535], [950, 565], [965, 550])
+    // Fetch stocks
+    const stocks = await fetchStocks();
+    console.log('Available Stocks:', stocks);
+
+    // Populate the stocks dropdown
+    fillStockDropdown(stocks);
+
+    // Hide the spinner after stocks are fetched
+    const spinner = document.querySelector('.spinner');
+    spinner.style.display = 'none';
+
+  } catch (error) {
+    console.error('Error initializing page:', error.message);
+    // Handle error on the front end (e.g., display an error message)
+    renderErrorMessage('Failed to initialize the page. Please try again.');
+  }
+}
+
+// Function to fetch stock data for the selected stock
+async function fetchStockData() {
+  const selectedStock = document.getElementById('stockDropdown').value;
+  console.log('Fetching data for', selectedStock);
+
+  try {
+    // Display the spinner while stock data is being fetched
+    const spinner = document.querySelector('.spinner');
+    spinner.style.display = 'inline-block';
+
+    // Fetch stock data from the server
+    const response = await fetch(`/stocks/${selectedStock}`);
+    const data = await response.json();
+    const stockData = await getStockData(selectedStock);
+
+    const x = d3.scaleUtc()
+      .domain([d3.min(stockData, d => new Date(d.timestamp)), d3.max(stockData, d => new Date(d.timestamp))])
+      .range([margin.left, width - margin.right]);
+
+    const y = d3.scaleLinear()
+      .domain([d3.min(stockData, d => d.value), d3.max(stockData, d => d.value)])
+      .range([height - margin.bottom, margin.top]);
+
+    const line = d3.line()
+      .x(d => x(new Date(d.timestamp)))
+      .y(d => y(d.value));
+
+    svg.selectAll('g').remove(); // Remove existing elements
+
+    // Update the SVG content (line chart)
+    svg.append('path')
+      .datum(stockData)
+      .attr('fill', 'none')
+      .attr('stroke', 'steelblue')
+      .attr('stroke-width', 3)
+      .attr('d', line);
+
+    // Add the x-axis.
+    svg.append('g')
+      .attr('transform', `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x));
+
+    // Add the y-axis.
+    svg.append('g')
+      .attr('transform', `translate(${margin.left},0)`)
+      .call(d3.axisLeft(y));
+
+    // Render stock data in the table
+    renderStockData(data);
+
+  } catch (error) {
+    console.error('Error fetching stock data:', error.message);
+
+    // Handle error on the front end (e.g., display an error message)
+    renderErrorMessage('Failed to retrieve stock data. Please try again.');
+  } finally {
+    // Hide the spinner once stock data is fetched or on error
+    const spinner = document.querySelector('.spinner');
+    spinner.style.display = 'none';
+  }
+}
+
+// Function to render stock data in the table
+function renderStockData(stockData) {
+  const tableBody = document.querySelector('#stockDataTable tbody');
+
+  // Clear existing rows
+  tableBody.innerHTML = '';
+
+  // Check if stockData is an array
+  if (!Array.isArray(stockData)) {
+    // If not an array, display an error message
+    renderErrorMessage('Failed to retrieve stock data. Please try again.');
+    return;
+  }
+
+  // Populate the table with stock data
+  stockData.forEach(entry => {
+    const row = document.createElement('tr');
+    const timestampCell = document.createElement('td');
+    const valueCell = document.createElement('td');
+
+    timestampCell.textContent = entry.timestamp;
+    valueCell.textContent = entry.value;
+
+    row.appendChild(timestampCell);
+    row.appendChild(valueCell);
+    tableBody.appendChild(row);
+  });
+}
+
+// Function to render error message on the front end
+function renderErrorMessage(message) {
+  const errorMessageContainer = document.getElementById('errorMessage');
+  errorMessageContainer.textContent = message;
+  errorMessageContainer.style.color = 'red';
+}
+
+// Call the initializePage function when the page loads
+window.onload = initializePage;
+
+async function getStockData(stockName) {
+  try {
+    const response = await fetch(`/stocks/${stockName}`);
+    const data = await response.json();
+
+    return data;
+  } catch (e) {
+    console.error('There is an error fetching your stock data:\n', e.message);
+  }
+}
